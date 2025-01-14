a5bf905b295ccada23d0e0fb4f61d726271e24e9
Shehan Garusingarachchige <shehangarusinghe@gmail.com>
2025-01-14 13:36:25 +0530

    router changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom"; // Import Routes and Route
 import Home from "./pages/Home";
 import About from "./pages/About";
-
 import Join from "./pages/Join";
-
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Explore from "./pages/Explore";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Routes> 
-          <Route path="/" element={<Home />} />   
-          <Route path="/about" element={<About />} />
-          
-          <Route path="/join" element={<Join />} /> 
-          <Route path="/explore" element={<Explore />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/explore" element={<Explore />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter here
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/VentureTalks">
-      <App />
+    <BrowserRouter>
+      <App /> {/* App does not need BrowserRouter anymore */}
     </BrowserRouter>
   </React.StrictMode>
 );
+
+
+
