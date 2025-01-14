5473d6352c6da67484726ac8ffe3ac821e0d896e
Shehan Garusingarachchige <shehangarusinghe@gmail.com>
2025-01-14 12:15:45 +0530

    basepath

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import './index.css';
 import App from './App';
 
 
-ReactDOM.render(
-  <BrowserRouter basename="/VentureTalks">
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter basename="/VentureTalks">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
-
-
